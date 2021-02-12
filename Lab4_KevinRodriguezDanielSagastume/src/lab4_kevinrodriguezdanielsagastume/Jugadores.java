523e86c594a917ff8aeaa4977728cd64dc4e9007
kevin09102001 <kevin31344@unitec.edu>
2021-02-12 13:55:07 -0600

    a

---
+++ Lab4_KevinRodriguezDanielSagastume/src/lab4_kevinrodriguezdanielsagastume/Jugadores.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4_kevinrodriguezdanielsagastume;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Jugadores extends equipos{
+    protected String Nombre, Apodo, NumerodeCamiseta, EquipodeFutbolFavorito, EquipodeBaloncesto_Favorito, JugadorFavorito, mayor_edad;
+    protected int año_Nacimiento,numero_estrellas;
+    
+    
+}
